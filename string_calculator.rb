2e50599f9b401548627dd759321a4ab6bfa6ceb8
kshitiz <kshitiz@nayantech.com>
2025-01-02 09:06:01 +0530

    Refactor: added the method for the string calculator as per the conditions given

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,3 +1,24 @@
 class StringCalculator
-  
+  def add(numbers)
+    return 0 if numbers.empty?
+
+    delimiter = ","
+    if numbers.start_with?("//")
+      delimiter_section, numbers = numbers.split("\n", 2)
+      delimiter = delimiter_section[2..]
+    end
+
+    numbers = numbers.gsub("\n", delimiter)
+    nums = numbers.split(delimiter).map(&:to_i)
+
+    negatives = nums.select { |n| n < 0 }
+    raise "negative numbers not allowed #{negatives.join(', ')}" if negatives.any?
+
+    nums.sum
+  end
 end
+
+# testing of the function
+p StringCalculator.new.add('')
+p StringCalculator.new.add('1')
+p StringCalculator.new.add('1,5')
